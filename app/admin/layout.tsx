0d96e1326c6bd32f54fe7c1d6523e9ee88a4c29f
Sebailla <seba.illa.1977@gmail.com>
2025-08-25 01:00:36 -0300

    feat: create AdminPage component with budget management UI and layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,33 @@
+import { ToastNotification } from "@/components";
+import DarkLogo from "@/components/ui/logos/DarkLogo";
+import Link from "next/link";
+
+export default async function AdminLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <header className='bg-purple-950 py-5'>
+                <div className='max-w-5xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
+                    <div className='w-96'>
+                        <Link href={'/admin'}>
+                            <DarkLogo />
+                        </Link>
+                    </div>
+                </div>
+            </header>
+            <section className='max-w-5xl mx-auto mt-20 p-3 py-10'>
+                {children}
+            </section>
+            <ToastNotification />
+
+            <footer className='py-5'>
+                <p className='text-center'>
+                    Todos los Derechos Reservados {new Date().getFullYear()}
+                </p>
+            </footer>
+        </>
+    );
+}
